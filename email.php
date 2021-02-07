b871ea092526b261fafd42580665839ef43d1d1f
hectorAlonso107 <78570274+hectorAlonso107@users.noreply.github.com>
2021-02-07 20:10:19 -0300

    html files

---
+++ email.php
@@ -0,0 +1,26 @@
+<?php
+     
+     $nombre = $_POST['nombre'];
+     $mail = $_POST['email'];
+     $empresa = $_POST['mensaje'];
+     $numero = $_POST['numero'];
+     
+     $header = 'From: ' . $mail . " \r\n";
+     $header .= "X-Mailer: PHP/" . phpversion() . " \r\n";
+     $header .= "Mime-Version: 1.0 \r\n";
+     $header .= "Content-Type: text/plain";
+     
+     $mensaje = "Este mensaje fue enviado por " . $nombre . ",\r\n";
+     $mensaje .= "Su e-mail es: " . $mail . " \r\n";
+     $mensaje .= "Su número telefónico es: " . $numero . " \r\n";
+     $mensaje .= "Mensaje: " . $_POST['mensaje'] . " \r\n";
+     $mensaje .= "Enviado el " . date('d/m/Y', time());
+     
+     $para = 'akealonso@gmail.com';
+     $asunto = $_POST['asunto'];
+     
+     mail($para, $asunto, utf8_decode($mensaje), $header);
+     
+     header("Location:index.html");
+     
+?>
